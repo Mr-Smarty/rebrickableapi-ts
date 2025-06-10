bb87dcae661d65c29f73900bd0bc269746d50ee4
Mr-Smarty <69656599+Mr-Smarty@users.noreply.github.com>
2025-06-10 08:41:19 +0000

    fix(types): update PartColor interface with correct properties and docs

--- packages/types/data/part-color.ts
+++ packages/types/data/part-color.ts
@@ -1,13 +1,24 @@
-// PartColor type for /api/v3/lego/parts/${string}/colors/
+/**
+ * Part Category (/api/v3/lego/parts/{part_num}/colors/).
+ *
+ * @see https://rebrickable.com/api/v3/docs
+ */
 export interface PartColor {
-  /** Color ID */
-  id: number;
-  /** Color name */
-  name: string;
-  /** RGB hex string */
-  rgb: string;
-  /** Is transparent */
-  is_trans: boolean;
-  /** List of element IDs for this part/color */
-  elements: string[];
+  /** The part color ID */
+	color_id: number;
+
+  /** The part color name */
+	color_name: string;
+
+  /** Number of sets that use this part/color combination */
+	num_sets: number;
+
+  /** Number of parts in sets that use this part/color combination */
+	num_set_parts: number;
+
+  /** The part image URL */
+	part_img_url: string;
+
+  /** Elements IDs for this part/color combination */
+	elements: string[];
 }
